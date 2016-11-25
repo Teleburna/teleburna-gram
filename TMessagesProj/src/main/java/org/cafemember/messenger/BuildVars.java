0edc09a53d70ec24d3dd150aa20f0c5fa18120ab
Teleburna <Teleburna@users.noreply.github.com>
2016-11-25 09:22:56 +0330

    Update BuildVars.java

--- TMessagesProj/src/main/java/org/cafemember/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/cafemember/messenger/BuildVars.java
@@ -13,9 +13,9 @@ public class BuildVars {
     public static int BUILD_VERSION = 9001;
     public static String BUILD_VERSION_STRING = "1.1";
 //    public static int APP_ID = 35292; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static int APP_ID = 70617;
+    public static int APP_ID = 22010;
 //    public static String APP_HASH = "73a30e590ee72edc14e6117aeaa7149a"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = "858a24ceb59bda5619fd0e0ac93ab0be";
+    public static String APP_HASH = "93d153142fa6df2c4a732ffdfe20b68b";
     public static String HOCKEY_APP_HASH = "73a30e590ee72edc14e6117aeaa7149a";
     public static String HOCKEY_APP_HASH_DEBUG = "73a30e590ee72edc14e6117aeaa7149a";
     public static String GCM_SENDER_ID = "760348033672";
@@ -23,5 +23,5 @@ public class BuildVars {
     public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
     public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
     public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
-    public static String FOURSQUARE_API_VERSION = "20150326";
+    public static String FOURSQUARE_API_VERSION = "20160326";
 }
